ae0ab283c281ecbbc1602e92dc85f48cd635845f
Francisco Marco-Serrano <support@matterof.biz>
2015-05-31 13:03:29 +0100

    updates

--- functions.R
+++ functions.R
@@ -1,1 +1,48 @@
+###############################################
+# Functions for the West Nile Virus challenge #
+###############################################
+
+# Generate predictions and submission file
+gensubmission <- function(whichmodel, newdata, type = "response"){
+  # which model is the model to use for the prediction
+  # newdata is the test data (variable names have to coincide with train names)
+  # type is the prediction type (response, terms)
+  virus <- predict(whichmodel, newdata, type)
+  sub.dt <- cbind.data.frame(Id = newdata$Id, WnvPresent = virus)
+  if (identifyNA(sub.dt) == 0) {
+    write.csv(sub.dt, file = "submission.csv", row.names = FALSE, quote = FALSE)
+    message("Submission file created. Expected RMSE is:")
+    #testRMSLE(sub.dt, count, whichmodel)
+  } else {
+    message("There are NAs and/or negative values in the predicted set!")
+    sub.dt[!complete.cases(sub.dt),]
+    sub.dt[sub.dt$count < 0, ]
+  }  
+}
+
+# Test RMSLE
+testRMSLE <- function(dt, y, whichmodel){
+  # dt is the data table
+  # y is the variable to test against the predicted one
+  # which model is the model to use for the prediction
+  require(Metrics)
+  
+  # ...with own data (not recommended)
+  myrmsle <- rmsle(dt[, y], fitted(whichmodel))
+  
+  # todo: split data frame to do in-sample testing
+  
+  return(myrmsle)
+  
+}
+
+# Identify NAs and Negative values
+identifyNA <- function(dt){
+  # dt is the data table
+  nas <- nrow(!complete.cases(dt))
+  neg <- nrow(dt[dt$count < 0, ])
+  unfit <- ifelse(length(nas) == 0, 0, nas) +
+    ifelse(length(neg) == 0, 0, neg)
+  return(unfit)
+}
 
--- main.R
+++ main.R
@@ -2,27 +2,50 @@
 # Load requirements
 #############################################
 
-pkg <- c("data.table")
+library(data.table)
 
-for (i in pkg) {
-  if(!require(i, character.only = TRUE)) install.packages(i, character.only = TRUE)
-  library(i, character.only = TRUE)
-}
-
+setwd("~/GitHub/predict-west-nile-virus")
 source("functions.R") # load functions
 
 #############################################
 # Read data
 #############################################
 
+temp <- unzip(paste(getwd(), "/data/train.csv.zip", sep=""))
+train.dt <- read.table(temp, header=TRUE, sep=",")
 
+temp <- unzip(paste(getwd(), "/data/test.csv.zip", sep=""))
+test.dt <- read.table(temp, header=TRUE, sep=",")
+
+temp <- unzip(paste(getwd(), "/data/spray.csv.zip", sep=""))
+spray.dt <- read.table(temp, header=TRUE, sep=",")
+
+temp <- unzip(paste(getwd(), "/data/weather.csv.zip", sep=""))
+weather.dt <- read.table(temp, header=TRUE, sep=",")
+
+rm(temp) # Remove temp object from memory
+
+mapdata <- readRDS(paste(getwd(), "/data/mapdata_copyright_openstreetmap_contributors.rds", sep=""))
 
 #############################################
 # Clean and transform data
 #############################################
 
+keep <- c(1,3,6,8,9,10,11,12)
+train.dt <- train.dt[,keep]
+
+keep <- c(1,2,4,5,7,9,10,11)
+test.dt <- test.dt[,keep]
 
 
 #############################################
 # Explore data
 #############################################
+
+
+
+
+#############################################
+# Models
+#############################################
+
